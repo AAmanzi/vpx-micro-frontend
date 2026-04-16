a69b54df393fcacc51ee4cc72d997f215e55bb18
aamanzi <alnick.amanzi@gmail.com>
2026-04-16 20:22:15 +0200

    patch startVpxTable: change startup flag so ESC opens options

--- src/electron/utils/startVpxTable.ts
+++ src/electron/utils/startVpxTable.ts
@@ -25,7 +25,7 @@ export const startVpxTable = async (
     '-DisableTrueFullScreen',
     '-primary',
     '-LessCPUthreads',
-    '-minimized',
+    '-ExtMinimized',
     '-play',
     tablePath,
   ];
